2bc8a80f53562506ffb9faa49492705eaa307694
jsbd10 <jsbenavides69@ucatolica.edu.co>
2020-06-07 20:08:29 -0500

    version 1

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template, request
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/cuenta')
+def cuenta():
+    return render_template('cuenta.html')
+
+
+@app.route('/recicla')
+def recicla():
+    return render_template('recicla.html')
+
+
+if __name__ == "__main__":
+    app.run()
